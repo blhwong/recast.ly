2c724084eb5bd11489c9a92343dd4ea8aabb395a
Brandon Wong <b.lh.wong@gmail.com>
2017-02-23 00:54:02 -0800

    Pass all tests through App

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,28 +1,46 @@
-class VideoListEntry extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  update(event) {
-    debugger;
-    console.log('click');
-    console.log(event);
-    console.log(this);
-    this.props.onUpdate(this.props.video);
-  }
-  render() {
-    var dataSnippet = this.props.video.snippet;
+// class VideoListEntry extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   update(event) {
+//     debugger;
+//     console.log('click');
+//     console.log(event);
+//     console.log(this);
+//     this.props.onUpdate(this.props.video);
+//   }
+//   render() {
+//     var dataSnippet = this.props.video.snippet;
+//     return (
+//       <div className="video-list-entry">
+//         <div className="media-left media-middle">
+//           <img className="media-object" src={dataSnippet.thumbnails.default.url} alt="" />
+//         </div>
+//         <div className="media-body">
+//           <div className="video-list-entry-title" onClick ={this.update.bind(this)}>{dataSnippet.title}</div>
+//           <div className="video-list-entry-detail">{dataSnippet.description}</div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+var VideoListEntry = (props) => {
+
+  var update = () => {
+    props.onUpdate(props.video);
+  };
     return (
       <div className="video-list-entry">
         <div className="media-left media-middle">
-          <img className="media-object" src={dataSnippet.thumbnails.default.url} alt="" />
+          <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" />
         </div>
         <div className="media-body">
-          <div className="video-list-entry-title" onClick ={this.update.bind(this)}>{dataSnippet.title}</div>
-          <div className="video-list-entry-detail">{dataSnippet.description}</div>
+          <div className="video-list-entry-title" onClick ={update}>{props.video.snippet.title}</div>
+          <div className="video-list-entry-detail">{props.video.snippet.description}</div>
         </div>
       </div>
-    );
-  }
+      );
 }
 
 // PropTypes tell other developers what `props` a component expects
