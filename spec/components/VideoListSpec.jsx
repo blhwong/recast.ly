545f5ae20b71a96d4bdea0237b3eea5d9e5d751c
Brandon Wong <b.lh.wong@gmail.com>
2017-02-22 22:50:35 -0800

    Change tests back to stateless

--- spec/components/VideoListSpec.jsx
+++ spec/components/VideoListSpec.jsx
@@ -6,8 +6,8 @@ describe('VideoList', function() {
   // only a single level deep. This isolates the component from it's children
   // See more: https://facebook.github.io/react/docs/test-utils.html#shallow-rendering
 
-  it('should be a dynamic functional component', function() {
-    expect(React.Component.isPrototypeOf(VideoList)).to.be.true;
+  it('should be a stateless functional component', function() {
+    expect(React.Component.isPrototypeOf(VideoList)).to.be.false;
   });
 
   it('should render one `VideoListEntry` when given one video', function() {
@@ -30,7 +30,7 @@ describe('VideoList', function() {
     shallowRenderer.render(
       <VideoList videos={threeFakeVideos} />
     );
-    
+
     var videoList = shallowRenderer.getRenderOutput();
     expect(videoList.props.children).to.have.length(3);
     videoList.props.children.forEach(child => expect(child.type).to.equal(VideoListEntry));
@@ -43,7 +43,7 @@ describe('VideoList', function() {
     shallowRenderer.render(
       <VideoList videos={fiveFakeVideos} />
     );
-    
+
     var videoList = shallowRenderer.getRenderOutput();
     expect(videoList.props.children).to.have.length(5);
     videoList.props.children.forEach(child => expect(child.type).to.equal(VideoListEntry));
